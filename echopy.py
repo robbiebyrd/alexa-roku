b15cfa1bf4154a6015ac3925a5f3e320007d98bc
Robbie Byrd <me@robbiebyrd.com>
2015-08-28 15:18:40 -0500

    Add SSL

--- echopy.py
+++ echopy.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
 #################################################
-#		Alexa Skills Kit Hello World			#
+#        Alexa Skills Kit Hello World            #
 #################################################
-# Zachary Priddy - 2015 						#
-# me@zpriddy.com 								#
-#												#
-# Features: 									#
+# Zachary Priddy - 2015                         #
+# me@zpriddy.com                                 #
+#                                                #
+# Features:                                     #
 #################################################
 #################################################
 
@@ -14,32 +14,32 @@ import echopy_app
 import echopy_doc
 import echopy_roku as myApp
 from flask import Flask, render_template, Response, send_from_directory, request, current_app, redirect, jsonify, json
-
+from OpenSSL import SSL
+import socketserver
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return echopy_doc.main_page
+    return echopy_doc.main_page
 
 
 @app.route("/EchoPyAPI",methods = ['GET','POST'])
 def apicalls():
-	if request.method == 'POST':
-		data = request.get_json()
-		print("POST")
-		sessionId = myApp.data_handler(data)
-		return sessionId + "\n"
+    if request.method == 'POST':
+        data = request.get_json()
+        print("POST")
+        sessionId = myApp.data_handler(data)
+        return sessionId + "\n"
 
 
 
 
 def run_echopy_app():
-	import socketserver
-	socketserver.ThreadingTCPServer.allow_reuse_address = True
-	echopy_app.run(app)
+    socketserver.ThreadingTCPServer.allow_reuse_address = True
+    echopy_app.run(app)
 
 
 if __name__ == "__main__":
-	myApp.data_init()
-	run_echopy_app()
+    myApp.data_init()
+    run_echopy_app()
