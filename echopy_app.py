a34b399d22c9f63d6b84e16c2af7dd0849d98146
Robbie Byrd <me@robbiebyrd.com>
2015-08-28 15:18:42 -0500

    Add SSL

--- echopy_app.py
+++ echopy_app.py
@@ -11,8 +11,13 @@
 def run(app):
 
     try:
+        from OpenSSL import SSL
+        context = SSL.Context(SSL.SSLv23_METHOD)
+        context.use_privatekey_file('/etc/pki/tls/certs/star.robbiebyrd.com.key')
+        context.use_certificate_file('/etc/pki/tls/certs/star.robbiebyrd.com.crt')
         app.run(debug=True,
                 port=5000,
+                ssl_context=context,
                 threaded=True,
                 use_reloader=False,
                 use_debugger=True,
