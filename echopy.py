405956300aebe97fa96bed9bd5f8b54c389a6dc7
Robbie Byrd <me@robbiebyrd.com>
2015-08-31 18:33:48 -0500

    Added testing, daemon check

--- echopy.py
+++ echopy.py
@@ -18,13 +18,7 @@ from OpenSSL import SSL
 import socketserver
 import sys
 import time
-from daemon import Daemon
-
-class MyDaemon(Daemon):
-        def run(self):
-                myApp.data_init()
-                run_echopy_app()
-
+import daemon
 
 app = Flask(__name__)
 
@@ -50,18 +44,6 @@ def run_echopy_app():
 
 
 if __name__ == "__main__":
-    daemon = MyDaemon('/tmp/daemon-example.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
-    else:
-            print("usage: %s start|stop|restart" % sys.argv[0])
-            sys.exit(2)
+    with daemon.DaemonContext():
+    myApp.data_init()
+    run_echopy_app()
